0e50eda925d254988f5dcd7c9e8538baca3aac71
SHoang6702 <shoang6702@gmail.com>
2024-01-16 14:57:14 -0600

    install.js to do complete

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -7,8 +7,18 @@ window.addEventListener('beforeinstallprompt', (event) => {
  butInstall.classList.toggle('hidden',false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+//Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+   return;
+  }
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+  butInstall.classList.toggle('hidden', true);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+//Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+ window.deferredPrompt = null;
+});
